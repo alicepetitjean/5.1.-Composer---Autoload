f282cf6c9fe38de743926640395e61c7d0a03da3
alicepetitjean <alice.petitjean55@orange.fr>
2020-10-19 11:57:25 +0200

    version 1

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once __DIR__.'/../vendor/autoload.php';
+use App\wcs\hello;
+use HelloWorld\SayHello;
+
+echo "1er objet : <br/>";
+$obj = new hello();
+echo $obj->talk();
+echo "<br/>2éme objet : <br/>";
+$obj2 = new SayHello();
+echo $obj2->world(); 
+?>
